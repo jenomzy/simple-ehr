0f1fbf9ad4bf17198807dfcf5530c568f27cc9ef
Jenom Gimba <jenomzy25@gmail.com>
2024-10-16 21:00:53 +0100

    Added designations to Doctor model

--- models/Doctor.js
+++ models/Doctor.js
@@ -5,6 +5,10 @@ const doctorSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, default: "doctor" },
+  designation: {
+    type: String,
+    enums: ["consultant", "radiologist", "pharmacist", "lab"],
+  },
   patients: [{ type: mongoose.Schema.Types.ObjectId, ref: "Patient" }],
 });
 
